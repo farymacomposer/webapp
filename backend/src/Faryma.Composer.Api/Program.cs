4f55625549a1c4dfd2cb286f7a18bf0a73314669
Mnham <yurazamastsev@gmail.com>
2025-08-07 23:23:15 +0700

    fix

--- backend/src/Faryma.Composer.Api/Program.cs
+++ backend/src/Faryma.Composer.Api/Program.cs
@@ -4,6 +4,8 @@ using Faryma.Composer.Api.DependencyInjection;
 using Faryma.Composer.Api.Extensions;
 using Faryma.Composer.Api.Features.OrderQueueFeature;
 using Faryma.Composer.Core.DependencyInjection;
+using Faryma.Composer.Core.Features.AppSettings;
+using Faryma.Composer.Core.Features.OrderQueueFeature;
 using Microsoft.AspNetCore.Authorization;
 using Serilog;
 
@@ -55,8 +57,8 @@ namespace Faryma.Composer.Api
             app.MapControllers();
             app.MapHub<OrderQueueNotificationHub>("/api/OrderQueueNotificationHub");
 
-            //await app.Services.GetRequiredService<AppSettingsService>().Initialize();
-            //await app.Services.GetRequiredService<OrderQueueService>().Initialize();
+            await app.Services.GetRequiredService<AppSettingsService>().Initialize();
+            await app.Services.GetRequiredService<OrderQueueService>().Initialize();
             await app.RunAsync();
         }
     }
