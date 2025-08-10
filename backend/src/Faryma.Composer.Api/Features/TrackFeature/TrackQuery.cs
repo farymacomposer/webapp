08330c6b4c8e447b925876c883a6051f09f3c313
Rost <rostislavvorobyev@gmail.com>
2025-08-10 12:09:58 +0200

    GraphQL endpoint для каталога треков: добавлен GraphQL запрос для Track.

---
+++ backend/src/Faryma.Composer.Api/Features/TrackFeature/TrackQuery.cs
@@ -0,0 +1,29 @@
+﻿using Faryma.Composer.Core.Features.TrackFeature;
+using Faryma.Composer.Infrastructure.Entities;
+
+namespace Faryma.Composer.Api.Features.TrackFeature
+{
+    public class TrackQuery()
+    {
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<TrackDto> GetTracks([Service] TrackService trackService) =>
+            trackService.GetAll().Select(t => TrackDto.Map(t));
+
+        public TrackDto GetTrackById(
+            [Service] TrackService trackService,
+            [ID] int id)
+        {
+            try
+            {
+                Track track = trackService.Find(id);
+                return TrackDto.Map(track);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new GraphQLException(new Error(ex.Message));
+            }
+        }
+    }
+}
