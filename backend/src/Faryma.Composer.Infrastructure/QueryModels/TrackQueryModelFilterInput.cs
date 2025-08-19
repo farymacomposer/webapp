416cf1aa1c7b353a1a76bee38e97a8ed580fb7e4
Mnham <yurazamastsev@gmail.com>
2025-08-19 22:30:43 +0700

    fix

---
+++ backend/src/Faryma.Composer.Infrastructure/QueryModels/TrackQueryModelFilterInput.cs
@@ -0,0 +1,40 @@
+﻿using HotChocolate.Data.Filters;
+using HotChocolate.Types;
+
+namespace Faryma.Composer.Infrastructure.QueryModels
+{
+    /// <summary>
+    /// Параметры фильтрации для музыкального трека
+    /// </summary>
+    public sealed class TrackQueryModelFilterInput : FilterInputType<TrackQueryModel>
+    {
+        protected override void Configure(IFilterInputTypeDescriptor<TrackQueryModel> descriptor)
+        {
+            descriptor.BindFieldsExplicitly();
+
+            descriptor.Field(x => x.Title);
+            descriptor.Field(x => x.ReleaseYear);
+            descriptor.Field(x => x.CountryId);
+            descriptor.Field(x => x.HasReview);
+            descriptor.Field(x => x.ExtendedGenres);
+            descriptor.Field(x => x.Tags);
+            descriptor.Field(x => x.Artists);
+            descriptor.Field(x => x.GenreIds);
+        }
+    }
+
+    public sealed class Contains : StringOperationFilterInputType
+    {
+        protected override void Configure(IFilterInputTypeDescriptor descriptor) =>
+            descriptor.Operation(DefaultFilterOperations.Contains).Type<StringType>();
+    }
+
+    public sealed class Int : IntOperationFilterInputType
+    {
+        protected override void Configure(IFilterInputTypeDescriptor descriptor)
+        {
+            descriptor.Operation(DefaultFilterOperations.GreaterThanOrEquals).Type<IntType>();
+            descriptor.Operation(DefaultFilterOperations.LowerThanOrEquals).Type<IntType>();
+        }
+    }
+}
---
+++ backend/src/Faryma.Composer.Infrastructure/QueryModels/TrackQueryModelSortInput.cs
@@ -0,0 +1,18 @@
+﻿using HotChocolate.Data.Sorting;
+
+namespace Faryma.Composer.Infrastructure.QueryModels
+{
+    /// <summary>
+    /// Параметры сортировки для музыкального трека
+    /// </summary>
+    public sealed class TrackQueryModelSortInput : SortInputType<TrackQueryModel>
+    {
+        protected override void Configure(ISortInputTypeDescriptor<TrackQueryModel> descriptor)
+        {
+            descriptor.BindFieldsExplicitly();
+
+            descriptor.Field(x => x.LastReviewRating);
+            descriptor.Field(x => x.UserRating);
+        }
+    }
+}
