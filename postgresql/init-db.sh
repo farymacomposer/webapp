bafb678d227edb061200c7cc97d68f254c0723b2
Mnham <yurazamastsev@gmail.com>
2025-09-28 20:35:55 +0700

    fix

--- postgresql/init-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
-    SELECT * FROM pg_extension WHERE extname = 'pg_stat_statements';
-EOSQL
