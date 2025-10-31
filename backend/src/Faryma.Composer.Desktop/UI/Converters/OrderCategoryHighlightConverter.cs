2f1532ee29f54dd85af845c39749d9d24282c7e9
Mnham <yurazamastsev@gmail.com>
2025-10-31 12:19:27 +0700

    fix

---
+++ backend/src/Faryma.Composer.Desktop/UI/Converters/OrderCategoryHighlightConverter.cs
@@ -0,0 +1,34 @@
+﻿using Faryma.Composer.Infrastructure.Enums;
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+
+namespace Faryma.Composer.Desktop.UI
+{
+    /// <summary>
+    /// Конвертер для подсветки по категории заказа
+    /// </summary>
+    public sealed partial class OrderCategoryHighlightConverter : IValueConverter
+    {
+        private static readonly Dictionary<OrderCategoryType, SolidColorBrush> _categoryTypeBrushes = new()
+        {
+            [OrderCategoryType.Unspecified] = new SolidColorBrush(Colors.Gray),
+            [OrderCategoryType.OutOfQueue] = new SolidColorBrush(Colors.Gold),
+            [OrderCategoryType.Donation] = new SolidColorBrush(Colors.YellowGreen),
+            [OrderCategoryType.Debt] = new SolidColorBrush(Colors.SlateBlue),
+        };
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is OrderCategoryType categoryType && _categoryTypeBrushes.TryGetValue(categoryType, out SolidColorBrush? brush))
+            {
+                return brush;
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => DependencyProperty.UnsetValue;
+    }
+}
--- backend/src/Faryma.Composer.Desktop/UI/Converters/StringHashHighlightConverter.cs
+++ backend/src/Faryma.Composer.Desktop/UI/Converters/StringHashHighlightConverter.cs
@@ -1,29 +1,22 @@
-﻿using Faryma.Composer.Infrastructure.Enums;
-using Microsoft.UI;
+﻿using Faryma.Composer.Desktop.Utils;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Media;
 using Windows.UI;
-using ColorHelper = Faryma.Composer.Desktop.Utils.ColorHelper;
 
 namespace Faryma.Composer.Desktop.UI
 {
-    public sealed partial class HighlightConverter : IValueConverter
+    /// <summary>
+    /// Конвертер для подсветки на основе хэша строки
+    /// </summary>
+    public sealed partial class StringHashHighlightConverter : IValueConverter
     {
         private const double _saturation = 80;
         private const double _lightness = 60;
 
-        private static readonly Dictionary<OrderCategoryType, Color> _categoryTypeColors = new()
-        {
-            [OrderCategoryType.Unspecified] = Colors.Gray,
-            [OrderCategoryType.OutOfQueue] = Colors.Gold,
-            [OrderCategoryType.Donation] = Colors.YellowGreen,
-            [OrderCategoryType.Debt] = Colors.SlateBlue,
-        };
-
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            if (value is string str)
+            if (value is string str && !string.IsNullOrEmpty(str))
             {
                 int hue = Math.Abs(GetSimpleDeterministicHash(str)) % 360;
                 Color color = ColorHelper.FromHsl(hue, _saturation, _lightness);
@@ -31,22 +24,17 @@ namespace Faryma.Composer.Desktop.UI
                 return new SolidColorBrush(color);
             }
 
-            if (value is OrderCategoryType categoryType)
-            {
-                return new SolidColorBrush(_categoryTypeColors[categoryType]);
-            }
-
             return DependencyProperty.UnsetValue;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language) => DependencyProperty.UnsetValue;
 
-        private static int GetSimpleDeterministicHash(string s)
+        private static int GetSimpleDeterministicHash(string text)
         {
             unchecked
             {
                 int hash = 17;
-                foreach (char c in s)
+                foreach (char c in text)
                 {
                     hash = (hash * 31) + c;
                 }
