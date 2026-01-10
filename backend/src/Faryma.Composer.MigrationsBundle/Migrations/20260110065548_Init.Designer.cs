6637a6155183ca91a9304a83a216bff2facc9879
Mnham <yurazamastsev@gmail.com>
2026-01-10 13:56:01 +0700

    fix

--- backend/src/Faryma.Composer.MigrationsBundle/Migrations/20260110065548_Init.Designer.cs
+++ backend/src/Faryma.Composer.MigrationsBundle/Migrations/20260110065548_Init.Designer.cs
@@ -14,7 +14,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Faryma.Composer.MigrationsBundle.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260110063244_Init")]
+    [Migration("20260110065548_Init")]
     partial class Init
     {
         /// <inheritdoc />
