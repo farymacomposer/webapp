652ae2480276055c2189745a979c335babcc9214
Mnham <yurazamastsev@gmail.com>
2026-05-05 01:53:24 +0700

    fix

--- backend/src/Faryma.Composer.Contracts/Api/Globals.cs
+++ backend/src/Faryma.Composer.Contracts/Api/Globals.cs
@@ -3,5 +3,10 @@
     public static class Globals
     {
         public const string IdempotencyKey = "Idempotency-Key";
+
+        /// <summary>
+        /// Максимальная длительность трека 15 мин
+        /// </summary>
+        public const int MaxTrackDurationSeconds = 60 * 15;
     }
 }
