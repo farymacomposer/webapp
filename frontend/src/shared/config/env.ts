c717ca71454e73062202e346f2e63059102b8cb4
foreverwildpalms <102798444+foreverwildpalms@users.noreply.github.com>
2026-05-29 10:43:51 +0300

    update dynamic twitch parent domain (#127)
    * правки фронтенд
    
    * fix 29/05
    
    * fix bottom queue width
    
    * fix svg error

--- frontend/src/shared/config/env.ts
+++ frontend/src/shared/config/env.ts
@@ -1,4 +1,3 @@
 export const env = {
-  apiUrl: import.meta.env.VITE_URL,
-  domen: import.meta.env.VITE_URL?.replace(/^https?:\/\//, '').replace(/:\d+$/, ''),
+  domen: window.location.hostname,
 };
